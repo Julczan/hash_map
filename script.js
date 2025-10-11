13c4e896f2f06ea92ee9d5ed369b3da81e0eea71
Julczan <julek111222h@gmail.com>
2025-10-11 13:38:28 +0200

    hash function

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ class HashMap {
     this.loadFactor = 0.75;
     this.capacity = 16;
   }
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
 }
+
+const hashMap = new HashMap();
+
+console.log(hashMap.hash("Julek"));
